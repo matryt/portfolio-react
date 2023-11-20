8f5639537bee37af1018aefd95a66220f4789d73
Mathieu CUVELIER <mathieu.cuvelier@outlook.fr>
2023-11-20 20:50:18 +0100

    Add styling to App component and update navigation menu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,7 @@
 import "../styles/Nav.css";
 import menu from "../img/menu.svg";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretRight } from '@fortawesome/free-solid-svg-icons';
 
 export default function Nav() {
     return (
@@ -7,15 +9,9 @@ export default function Nav() {
             <div id="menu-container">
             <img src={menu} height="30px" alt="menu"></img>
             <ul>
-                <li>
-                    <a href="/">Accueil</a>
-                </li>
-                <li>
-                    <a href="/projects">Projets</a>
-                </li>
-                <li>
-                    <a href="/contact">Contact</a>
-                </li>
+                <a href="/"><li><FontAwesomeIcon icon={faCaretRight}/>Accueil</li></a>
+                <a href="/projects"><li><FontAwesomeIcon icon={faCaretRight}/>Projets</li></a>
+                <a href="/contact"><li><FontAwesomeIcon icon={faCaretRight}/>Contact</li></a>
             </ul>
             </div>
         </nav>
